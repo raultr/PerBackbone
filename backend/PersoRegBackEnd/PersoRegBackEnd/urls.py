be18c43a51e3609dae34f7f4bc1490d20cfc2855
Raul Torres <raultr@gmail.com>
2014-11-21 23:23:59 -0600

    inicio del backend

---
+++ backend/PersoRegBackEnd/PersoRegBackEnd/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'PersoRegBackEnd.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('catalogos.urls')),
+)
+
+
+if settings.DEBUG:
+	urlpatterns += patterns('',
+	url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.MEDIA_ROOT,}),)
